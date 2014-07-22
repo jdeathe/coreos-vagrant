d3b326bc227a0eaadac392f7cda94a1a9300800c
James Deathe <james@deathe.org>
2014-07-22 23:37:15 +0100

    BUG: Fixed an issue with '$' being appended to lines added to the /etc/hosts.
    Replaced "sudo sh -c" with "sudo bash -c" since the setup-hosts.sh script is written in bash.

--- setup-hosts.sh
+++ setup-hosts.sh
@@ -7,17 +7,17 @@ if [[ $DIR_PATH == */* ]]; then
 fi
 
 # Clean up any existing entries
-sudo sh -c "sed -i -e '/# CoreOS Vagrant$/d' /etc/hosts"
-sudo sh -c "sed -i -e '/172.17.8.10[1-9]*	core-0[1-9]* core-0[1-9]*.local$/d' /etc/hosts"
-sudo sh -c "sed -i -e '/172.17.8.10[1-9]*	app-[0-9]* app-[0-9]*.local$/d' /etc/hosts"
+sudo bash -c "sed -i -e '/# CoreOS Vagrant$/d' /etc/hosts"
+sudo bash -c "sed -i -e '/172.17.8.10[1-9]*	core-0[1-9]* core-0[1-9]*.local$/d' /etc/hosts"
+sudo bash -c "sed -i -e '/172.17.8.10[1-9]*	app-[0-9]* app-[0-9]*.local$/d' /etc/hosts"
 
 # Add coreos vagrant hosts
-sudo sh -c "echo $'# CoreOS Vagrant' >> /etc/hosts"
-sudo sh -c "echo $'172.17.8.101\tcore-01 core-01.local' >> /etc/hosts"
-sudo sh -c "echo $'172.17.8.102\tcore-02 core-02.local' >> /etc/hosts"
-sudo sh -c "echo $'172.17.8.103\tcore-03 core-03.local' >> /etc/hosts"
+sudo bash -c "echo $'# CoreOS Vagrant' >> /etc/hosts"
+sudo bash -c "echo $'172.17.8.101\tcore-01 core-01.local' >> /etc/hosts"
+sudo bash -c "echo $'172.17.8.102\tcore-02 core-02.local' >> /etc/hosts"
+sudo bash -c "echo $'172.17.8.103\tcore-03 core-03.local' >> /etc/hosts"
 
 # Add app hosts
-sudo sh -c "echo $'172.17.8.101\tapp-1 app-1.local' >> /etc/hosts"
+sudo bash -c "echo $'172.17.8.101\tapp-1 app-1.local' >> /etc/hosts"
 
 echo "Done."
